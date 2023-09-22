f5f0b932ed76c12af22b8e27bd291bcbe1fc81c6
liam02walker <17liamwalker02@gmail.com>
2023-09-22 10:57:58 +0100

    test 3 done

--- js/app.js
+++ js/app.js
@@ -53,10 +53,19 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) {}
+function sumAndMultiply(a, b, c) {
+  let sumAB = sum(a, b)[0];
+  let sumABC = sum(sumAB, c)[0];
+  let productAB = multiply(a, b)[0];
+  let productABC = multiply(productAB, c)[0];
+  let messageA = `${a} and ${b} and ${c} sum to ${sumABC}.`;
+  let messageB = `The product of ${a} and ${b} and ${c} is ${productABC}.`;
+
+  return [sumABC, productABC, messageA, messageB];
+}
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
